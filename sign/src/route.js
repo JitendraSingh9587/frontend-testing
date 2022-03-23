b68b1895ac7865059c51fc205b8b8f95873130ce
Jitendra <jitendrasinghrao9587@gmail.com>
2022-03-23 11:27:32 +0530

    route configured with foreget password

--- sign/src/route.js
+++ sign/src/route.js
@@ -2,12 +2,16 @@ import ReactDOM from 'react-dom';
 import { Routes, Route } from 'react-router-dom';
 import SignIn from './components/sign-in/sign-in';
 import Form from './components/sign-up/Form';
+import ForgetPassword from './components/forgetpass/forgetPassword';
+import ResetPassword from './components/forgetpass/reset-password';
 
 export default function Router() {
   return (
     <Routes>
       <Route path='/signin' element={<SignIn />} />
       <Route path='/signup' element={<Form />} />
+      <Route path='/forgetPassword' element={<ForgetPassword />} />
+      <Route path='/resetpassword' element={<ResetPassword />} />
     </Routes>
   );
 }
